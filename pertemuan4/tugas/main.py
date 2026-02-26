b1a55fcdee4ebb5efbcac16572521c868d3d6c36
fachrurrachmanh <hkeemrach@gmail.com>
2026-02-27 02:10:48 +0700

    upload tugas praktikum 4

--- pertemuan4/tugas/main.py
+++ pertemuan4/tugas/main.py
@@ -2,31 +2,28 @@ from tabulate import tabulate
 import kurs
 import konverter
 
-def main():
-    print("=== KONVERTER MATA UANG ===")
+
     
     
-    tabel = [[k, f"{v:,}"] for k, v in kurs.kurs.items()]
-    print(tabulate(tabel, headers=["Kode", "Kurs"], tablefmt="grid"))
+tabel = [[k, f"{v:,}"] for k, v in kurs.kurs.items()]
+print(tabulate(tabel, headers=["Kode", "Kurs"], tablefmt="grid"))
     
  
-    dari = input("\nDari (IDR/USD/EUR/SGD/JPY): ").upper()
-    ke = input("Ke   (IDR/USD/EUR/SGD/JPY): ").upper()
+dari = input("\nDari (IDR/USD/EUR/SGD/JPY): ").upper()
+ke = input("Ke   (IDR/USD/EUR/SGD/JPY): ").upper()
     
-    try:
-        jumlah = float(input("Jumlah: "))
+try:
+    jumlah = float(input("Jumlah: "))
         
-        hasil = konverter.konversi(jumlah, dari, ke, kurs.kurs)
+    hasil = konverter.konversi(jumlah, dari, ke, kurs.kurs)
         
-        if dari == "IDR":
-            print(f"\nRp {jumlah:,.0f} = {hasil:.2f} {ke}")
-        else:
-            print(f"\n{jumlah} {dari} = Rp {hasil:,.0f}")
+    if dari == "IDR":
+        print(f"\nRp {jumlah:,.0f} = {hasil:.2f} {ke}")
+    else:
+        print(f"\n{jumlah} {dari} = Rp {hasil:,.0f}")
             
-    except ValueError:
-        print("Error: Masukkan angka yang valid!")
-    except KeyError:
-        print("Error: Kode mata uang tidak terdaftar!")
+except ValueError:
+    print("Error: Masukkan angka yang valid!")
+except KeyError:
+    print("Error: Kode mata uang tidak terdaftar!")
 
-if __name__ == "__main__":
-    main()
